4fcf035fc4588812db25f1f978b2e0a8d315fd52
David Reeves <davidsreeves1@gmail.com>
2019-07-02 11:03:04 -0500

    set up database connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 
+// Get the database pool from module
+const pool = require( './modules/pool' );
+
 // Setting up our express app
 const app = express();
 app.use(express.static('server/public'));
@@ -8,7 +11,9 @@ app.use( bodyParser.urlencoded({extended: true}));
 
 
 //Routes will go here
+app.get('/music', (req, res) => {
 
+})
 
 
 // Start up the server
