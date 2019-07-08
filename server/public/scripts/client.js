ae385a08bb64132b19e76adf9d03af12f023243f
David Reeves <davidsreeves1@gmail.com>
2019-07-08 11:46:39 -0500

    finished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,23 +5,58 @@ function onReady() {
     
     
     // TODO - Setup Form
-
+    $('#submit').on('click', handleSubmit);
 
     //TODO - get all music from server
     getAllMusic();
 }
 
 function getAllMusic() {
-
+    $('#music').empty();
     //Make ajax GET request to server
     $.ajax({
         method: 'GET',
         url: '/music'
     }).then(function(response){
         console.log('back from server', response);
+        for(let currentItem of response){
+            $('#music').append(`<tr><td>${currentItem.track}</td><td>${currentItem.artist}</td>
+            <td>${currentItem.published}</td><td>${currentItem.rank}</td></tr>`)
+        }
     }).catch( function(error){
         console.log('error with getting music', error);
         alert('Sorry, could not get music. Try again later.');
+    })
+}
+
+function handleSubmit() {
+    let newTrack = $('#track-in').val();
+    let newArtist = $('#artist-in').val();
+    let newRank = $('#rank-in').val();
+    let newPublished = $('#publish-in').val();
+
+    let newObject = {
+        rank: newRank,
+        track: newTrack,
+        artist: newArtist,
+        published: newPublished
+    }
+
+    
+    $.ajax({
+        method: 'POST',
+        url: '/music',
+        data: newObject
+    })
+    .then(function(response){
+        getAllMusic();
+        $('#track-in').val('');
+        $('#artist-in').val('');
+        $('#rank-in').val('');
+        $('#publish-in').val('');
+    })
+    .catch( (error) => {
+        console.log(error);
     })
 }
 
--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 
 // Get the database pool from module
-const pool = require( './modules/pool' );
-
+const pool = require('./modules/pool')
 // Setting up our express app
 const app = express();
 app.use(express.static('server/public'));
@@ -12,7 +11,34 @@ app.use( bodyParser.urlencoded({extended: true}));
 
 //Routes will go here
 app.get('/music', (req, res) => {
+    // SQL to get all music (aka songs)
+    const sqlText = 'SELECT * FROM "songs" ORDER BY "rank" DESC;';
+    pool.query(sqlText)
+        .then(response => {
+        console.log('Got info from database', response);
+        res.send(response.rows)
+    })
+        .catch( (error) => {
+        console.log('Error getting music from db', error);
+        res.sendStatus(5000);
+    })
+});
 
+app.post('/music', (req, res) => {
+    const newSong = req.body;
+    console.log('Adding new song', newSong);
+
+    const sqlText = `INSERT INTO "songs" ("rank", "track", "artist", "published")
+    VALUES ($1, $2, $3, $4);`;
+    const values = [Number(newSong.rank), newSong.track, newSong.artist, newSong.published];
+    pool.query(sqlText, values)
+        .then( (response) => {
+            res.sendStatus(201);
+        })
+        .catch( (error) => {
+            console.log('Error adding new song to db', error);
+            res.sendStatus(500);
+        })
 })
 
 
